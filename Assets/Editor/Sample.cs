78abae9fae1150c138ca76e2b2ee6bcec00d00b3
sassembla <toru.inoue@kissaki.tv>
2017-05-11 13:05:19 +0900

    うごいた。わーい。

--- Assets/Editor/Sample.cs
+++ Assets/Editor/Sample.cs
@@ -6,12 +6,11 @@ public class Smaple {
     [MenuItem("Window/test")] public static void Run () {
         // var dest = System.IO.Path.Combine(Application.persistentDataPath, "profiler.log");
         Profiler.logFile = "profiler.log";//dest;
-        // バイナリファイルを有効に( profiler.log.data )として保存します
-
         Profiler.enableBinaryLog = true;
-        
-        Profiler.enabled = !Profiler.enabled;
 
-        Debug.LogError("here! Profiler.enabled:" + Profiler.enabled);// ここがfalseになると動くんだけど。なんで？
+        // Profiler.enableBinaryLog = false;
+        Profiler.enabled = !Profiler.enabled;
+        
+        Debug.LogError("changed. Profiler.enabled:" + Profiler.enabled);// ここがfalseになると動くんだけど。なんで？
     }
 }
---
+++ Assets/Repo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class Repo : MonoBehaviour {
+
+	// Use this for initialization
+	// void Start () {
+	// 	// 複数のフラグを同時に上げ下げしないと正しく動作しない
+	// 	Profiler.logFile = "profiler.log";
+    //     Profiler.enableBinaryLog = true;
+    //     Profiler.enabled = true;
+
+    //     Debug.LogError("here! Profiler.enabled:" + Profiler.enabled);
+	// }
+	
+	// // Update is called once per frame
+	// void Update () {
+		
+	// }
+
+	// void OnApplicationQuit () {
+	// 	// 複数のフラグを同時に上げ下げしないと正しく動作しない
+	// 	Profiler.logFile = string.Empty;
+	// 	Profiler.enableBinaryLog = false;
+	// 	Profiler.enabled = false;
+	// 	Debug.LogError("exit! Profiler.enabled:" + Profiler.enabled);
+	// }
+}
