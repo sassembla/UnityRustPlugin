06f5ce2d3ece686d266e61a40335ef8af4d03a46
sassembla <toru.inoue@kissaki.tv>
2017-05-16 04:40:07 +0900

    バイナリの分解中

--- Assets/Editor/Sample.cs
+++ Assets/Editor/Sample.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Text;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Profiling;
@@ -7,11 +9,57 @@ public class Smaple {
 
     private const string controlPath = "control.log";
     private const string profilePath = "prof/prof_";
-    private const int filePerFrameLimit = 100;
+    private const int filePerFrameLimit = 2;
     
     private static bool running;
+
+    [MenuItem("Window/prof")] public static void Read () {
+        var dataFileProfilerName = "prof/prof_0002";
+        var dataFileName = dataFileProfilerName + ".data";
+        
+        var header = new byte[16];
+        
+        using (var fs = File.OpenRead(dataFileName)) {
+            while (true) {
+                // ヘッダサイズ16byte
+                fs.Read(header, 0, header.Length);
+
+                int size = GetIntValue(header, 8); // オフセットの前のとこは最初は固定値なんだけど、なんか情報を含んでそう。
+                Debug.LogError("size:" + size);
+                
+                // 1フレームのデータサイズがこれ。
+                var buffer = new byte[size];
+                var rest = fs.Read(buffer, 0, size);
+
+                // このデータに何がどうやって入っているのかを追うことができれば、幸せになれそうなんですが。
+                // たぶん同じようなフォーマットだと思うんだよね。
+                
+                Debug.LogError("a:" + Encoding.Default.GetString(buffer));
+                fs.Read(header, 0, header.Length);
+                // int size2 = GetIntValue(header, 8);
+                // Debug.LogError("size2:" + size2);
+
+                break;
+
+                if (0 < rest) {
+                    continue;
+                }
+                break;
+            }
+        }
+
+        Profiler.AddFramesFromFile(dataFileProfilerName);
+    }
+    private static int GetIntValue(byte[] bin, int offset) {
+        return (bin[offset + 0] << 0) +
+            (bin[offset + 1] << 8) +
+            (bin[offset + 2] << 16) +
+            (bin[offset + 3] << 24);
+    }
+
+
     [MenuItem("Window/test")] public static void Run () {
-        var dest = System.IO.Path.Combine(Application.persistentDataPath, profilePath);
+        var dest = Path.Combine(Application.persistentDataPath, profilePath);
         
         running = !running;
         
