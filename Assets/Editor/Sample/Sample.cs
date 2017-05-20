4ffca15935b95a068cd9744295ee485b42fced23
sassembla <toru.inoue@kissaki.tv>
2017-05-20 16:32:15 +0900

    解析継続中

--- Assets/Editor/Sample/Sample.cs
+++ Assets/Editor/Sample/Sample.cs
@@ -13,6 +13,43 @@ public class Smaple {
     
     private static bool running;
 
+
+    [InitializeOnLoadMethod] static void OnProjectLoadedInEditor () {
+        // ここで、吐き出したファイルの読み方をあれこれ試す。
+        // 文字で出すのがまず問題っぽいので、
+        /*
+        " ???+?!<>~$߻? 73??J???]73? ??t7??P
+ix
+
+
+
+!??????*? ?^bhlors?tu|}~???????????????$,-/06?
+           ???DzDzD?7.?	?????	Q	?@????UNKNOWNAudio ListenerMain Threadr??+?!)?o/?!
+                                                                                    ?0?!gN0?!?1?!?1?!?1?!?1?!2?!!2?!)2?!+2?!-2?!12?!32?!82?!82?!82?!;2?!<2?!<2?!=2?!=2?!>2?!?2?!A2?!A2?!C2?!2?!I2?!f2?!f2?!h2?!j2?!n2?!o2?!q2?!v2?!?2?!?2?!?2?!?2?!?2?!?2?!?2?!?2?!?2?!?2?!?3?!??3?!?W<?!
+                                                                                                              @[<?!_<?!m<?!n<?!n<?!o<?!t<?!u<?!{<?!
+|<?!
+?<?!?<?!?<?!?<?!?<?!?<?!?<?!?<?!?<??<?!?<?!?<?!?<?!?<?!?<?!?<?!?<?!?<?!e?<?!	?<?!#?<?!?<?!?<?!?<?!	?<?!?<?!?<?!?<?!?<?!?<?!?<?!?<?!?<?!?<?!?<?!=?!=?!=?!=?!"=?!,=?!.=?!/=?!0=?!1=?!5=?!6=?!6=?!^=?!`=?!g=?!2?b?!yd?UpdateScreenManagerAndInputGUIUtility.SetSkin()	WLoading.UpdateWebStreamLoading.UpdatePreloadingApplication.Integrate Assets in Background
+Cleanup Unused Cached DataGameView.GetMainGameViewTargetSize()	ProcessRemoteInput
+                                                                                  GlobalEventQueuePhysics.InterpolationAudioManager.FixedUpdateFixedBehaviourUpdatePhysics.StoreInterpolationPosesPhysics2D.FixedUpdatePhysics2D.MovementStatePhysics2D.SimulatePhysics2D.UpdateTransformsPhysics2D.EffectorFixedUpdatePhysics2D.JointBreakLimitsPhysics2D.CallbacksPhysics2D.ContactReportingPhysics2D.SimulatePhysics.InterpolationPhysics2D.DynamicUpdatePhysics2D.MovementStateHandleUtility.SetViewInfo()	Monobehaviour.OnMouse_SendMouseEvents.DoSendMouseEvents()	BehaviourUpdateDirectorUpdateNavMeshManagerNetwork.Update	ParticleSystem.UpdateLateBehaviourUpdateCanvas.SendWillRenderCanvases()	AudioManager.UpdatePhysics.UpdateClothParticleSystem.EndUpdateAllParticleSystem.Update2Substance.UpdateLightProbeProxyVolumeManager.UpdateRendering.UpdateDirtyRenderersMeshSkinning.UpdateMeshSkinning.PrepareGfx.WaitForPresentReflectionProbes.UpdateWaitForRenderJobsRenderTexture.SetActiveCamera.RenderCullingCamera.FireOnPreCull()	ScheduleCullingGroupsCullingGroupSendEventsRecalc BoundsPrepareSceneCullingParametersLOD.ComputeLODLOD.ComputeLODSceneCullingPrepareSceneNodesCullAllVisibleLightsFindActiveLightsAddDirectionalLightsFindDirectionalShadowCastingLightsFindShadowCastingLightsShadows.CullDirectionalShadowCastersAddActiveLocalLightsFindLocalShadowCastingLightsFindShadowCastingLightsCullSendEventsTerrain.Trees.OnWillRenderWaitForJobGroupCamera.FireOnPreRender()	UpdateDepthTextureRenderTexture.SetActiveRenderLoop.CleanupNodeQueueRenderTexture.SetActiveRenderTexture.SetActiveDrawingRender.PrepareRender.OpaqueGeometryRenderForwardOpaque.PrepareRenderForwardOpaque.RenderClearWaitForJobGroupRenderForward.RenderLoopJobBatchRenderer.FlushCamera.RenderSkyboxFindBrightestDirectionalLightMaterial.SetPassFastRenderLoop.CleanupNodeQueueRender.MotionVectorsCamera.ImageEffectsRenderLoop.CleanupNodeQueueRender.TransparentGeometryRenderForwardAlpha.PrepareRenderForwardAlpha.RenderRenderForward.RenderLoopJobBatchRenderer.FlushCamera.FireOnPostRender()	Flare.RenderCamera.ImageEffectsRenderLoop.CleanupNodeQueueCamera.GUILayerRenderLoop.CleanupNodeQueueDestroyCullResultsRenderLoop.CleanupNodeQueueRenderLoop.CleanupNodeQueueGUI.RepaintEvent.Internal_MakeMasterEventCurrent()	PlayerEndOfFrameProfiler.FinalizeAndSendFrameOverheadRender Thread?+?!
+ x/?!*?1?!,O3?!W3?!?3?!?3?!??;?!??<?!?<?!?<?!?<?!
+                                                 b=?!?b?!?c?!d?!Gfx.ProcessCommandsGfx.ProcessCommandsGfx.ProcessCommandsPutAllGeometryJobFenceGfx.ProcessCommandsGfx.SetRenderTargetGfx.ProcessCommandsGfx.ProcessCommandsGfx.SetRenderTargetGfx.SetRenderTargetGfx.SetRenderTargetGfx.ProcessCommandsGfx.ProcessCommandsGfx.ProcessCommandsGfx.Proce=?!IdleIdleIdleIdleUnity Job SystemWorker ThreadF?<?!)?<?+???+?!?<?!?<?!?<?!H?<?!(?<?!	=?!?,	=?!IdleIdleIdleIdleIdleIdleRenderForwardAlpha.SortIdleUnity Job SystemWorker Thread?+?!??+?!?<?!?<?!?<?!?<?!?<?!?<?!?<?!@?<?!?<?!?<?!?<?!?<?!*?<?!	=?!	=?!	=?!-
+=?!IdleSceneNodesInitJobPrepareSceneNodesSetUpPrepareSceneNodesSetUpPrepareSceneNodesSetUpPrepareSceneNodesSetUpPrepareSceneNodesSetUpPrepareSceneNodesCombineJobIdleRenderForwardOpaque.SortIdleIdleIdleIdleIdleIdleIdleIdlePreloadManagerWorker Thread????
+         */
+
+        //  とかなってるので、まずはhexでみてみよう。
+        // サイズに関連するところはバリッバリの0なので、それ以外の見方を探すか。固定長のような気がする。
+        // で、4000byteかっ飛ばしてみる。
+        using (var fs = File.OpenRead("exp")) {
+            var by = new byte[3000];
+
+            fs.Read(by, 0, by.Length);
+            Debug.LogError("この続きをcharに変換できる気がする。このあたりのフィールドになんかインデックス情報があるのかな。");
+
+            
+        }
+    }
+
+    
     [MenuItem("Window/readfile")] public static void Read () {
         var dataFileProfilerName = "prof/prof_0001";
         var dataFileName = dataFileProfilerName + ".data";
@@ -24,7 +61,7 @@ public class Smaple {
                 // ヘッダサイズ16byte
                 fs.Read(header, 0, header.Length);
 
-                int size = GetIntValue(header, 8); // オフセットの前のとこは最初は固定値なんだけど、なんか情報を含んでそう。
+                int size = GetIntValue(header, 8); // オフセットの前のとこは最初は固定値なんだけど、なんか情報を含んでそう。同じファイルに複数フレーム含んだ場合かな。
                 Debug.LogError("データのsize:" + size + " こんだけのサイズのデータがあるはず。");
 
                 
